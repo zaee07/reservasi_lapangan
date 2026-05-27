477a165c57a30afbc4d49bce3658fb524430b313
zaee07 <ihzasofyansah@gmail.com>
2026-05-28 05:22:01 +0700

    feat: helper status slot

---
+++ application/helpers/status_helper.php
@@ -0,0 +1,22 @@
+<?php
+
+function badge_status_slot($status)
+{
+    switch ($status) {
+
+        case STATUS_SLOT_AVAILABLE:
+            return '<span class="badge bg-label-success">Tersedia</span>';
+
+        case STATUS_SLOT_BOOKED:
+            return '<span class="badge bg-label-primary">Dipesan</span>';
+
+        case STATUS_SLOT_IN_USED:
+            return '<span class="badge bg-label-warning">In Use</span>';
+
+        case STATUS_SLOT_CLOSED:
+            return '<span class="badge bg-label-danger">Maintenance/Tutup</span>';
+
+        default:
+            return '<span class="badge bg-label-secondary">Unknown</span>';
+    }
+}
